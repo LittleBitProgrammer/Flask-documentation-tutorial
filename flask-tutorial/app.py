b73437e4b2a31b9553d45486822aa5da1fa0995d
Roberto <vecchioroberto93@gmail.com>
2020-04-06 14:51:25 +0200

    gitignore
    - added gitignore
    -added flask boilerplate

---
+++ flask-tutorial/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def index():
+    return 'Hello world'
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
